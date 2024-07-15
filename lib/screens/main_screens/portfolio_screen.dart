4369c0d052fadd1c4d056ae442ce757f1ff6e28f
Arjav Patel <f20231140@goa.bits-pilani.ac.in>
2024-07-16 01:08:08 +0530

    Commit #6

--- lib/screens/main_screens/portfolio_screen.dart
+++ lib/screens/main_screens/portfolio_screen.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:stock_math_final/services/components/typewriter_text.dart';
 
 import '../../models/portfolio_model.dart';
 import '../../services/components/portfolio_list.dart';
@@ -28,24 +27,23 @@ class _PortfolioScreenState extends State<PortfolioScreen> {
   @override
 
   Widget build(BuildContext context) {
-    late var portfolios = Provider.of<List<PortfolioModel>?>(context);
     return StreamProvider<List<PortfolioModel>?>.value(
       value: Historyservice().portfolio,
       initialData: null,
       child: Scaffold(
         appBar: AppBar(
           actions: [
-            Spacer(flex: 1,),
+            const Spacer(flex: 1,),
 
             Text('$username’s Portfolio',
             style: GoogleFonts.sourceCodePro(
               fontWeight: FontWeight.bold,
               fontSize: 20
             ),),
-            Spacer(flex: 5,)
+            const Spacer(flex: 5,)
           ],
         ),
-        body: PortfolioList()
+        body: const PortfolioList()
       ),
     );
   }
